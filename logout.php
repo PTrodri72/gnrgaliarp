549af4a702d17068783beee28da211e683840c7d
PTrodri72 <rodrigofilipe631@gmail.com>
2026-04-03 14:18:20 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Remove todas as variáveis da sessão
-session_destroy(); // Destrói a sessão no servidor
-header("Location: index.php?aba=inicio"); // Redireciona para a página inicial (pública)
-exit();
-?>
